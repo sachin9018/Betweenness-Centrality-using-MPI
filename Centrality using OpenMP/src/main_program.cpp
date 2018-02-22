a8be96dfe220b94378cbe0b1c3eb850a2d188c1c
ssgandham <gandhamsnehalsrinivas@gmail.com>
2018-02-22 13:30:12 -0800

    Centrality function has been modified

--- Centrality using OpenMP/src/main_program.cpp
+++ Centrality using OpenMP/src/main_program.cpp
@@ -72,7 +72,7 @@ void initialize(int V, int source_vertex, vector<int> predecessor[], int *sigma,
 //Method for calculating the Betweenness Centrality
 void calculate_centrality(int V, vector<int> adj[], vector<int> predecessor[],
 		int *sigma, int *distance, float *delta, stack<int> st,
-		map<int, vector<int> > map, queue<int> q) {
+		map<int, vector<int> > map, queue<int> q,float* CB,int** shortest_path_dist) {
 
 //	iterating through each vertex
 	for (int i = 1; i <= V; i++) {
@@ -85,7 +85,7 @@ void calculate_centrality(int V, vector<int> adj[], vector<int> predecessor[],
 			vertex = q.front();
 			q.pop();
 			st.push(vertex);
-			for ( auto &i : adj[vertex] ) {
+			for (auto &i : adj[vertex]) {
 
 				if (distance[i] < 0) {
 					q.push(i);
@@ -94,13 +94,31 @@ void calculate_centrality(int V, vector<int> adj[], vector<int> predecessor[],
 
 				if (distance[i] == distance[vertex] + 1) {
 					sigma[i] = sigma[i] + sigma[vertex];
-					bool found = (std::find(predecessor[i].begin(), predecessor[i].end(), vertex) != predecessor[i].end());
+					bool found = (std::find(predecessor[i].begin(),
+							predecessor[i].end(), vertex)
+							!= predecessor[i].end());
+//					https://stackoverflow.com/questions/24139428/check-if-element-is-in-the-list-contains
 					if (!found)
 						predecessor[i].push_back(vertex);
 				}
 			}
 
 		}
+
+        while (!st.empty()) {
+            int st_neigh = st.top();
+            st.pop();
+            for (auto &vertex_pred : predecessor[st_neigh]) {
+                float tmp_delta = delta[vertex_pred]
+                        + (((float) ((float) sigma[vertex_pred] / (float) sigma[st_neigh])) * (1 + delta[st_neigh]));
+                delta[vertex_pred] += tmp_delta;
+                if (source_vertex != st_neigh)
+                    CB[st_neigh] += delta[st_neigh];
+            }
+        }
+        //            System.out.println("i :" + i);
+		for(int i=0;i<V;i++) shortest_path_dist[source_vertex][i] = distance[i];
+//        shortest_path_dist[src] = distance;
 
 	}
 }
@@ -114,9 +132,16 @@ int main() {
 	int *sigma = new int[V];
 	int *distance = new int[V];
 	float *delta = new float[V];
+	float * BC = new float[V];
 	stack<int> st;
 	map<int, vector<int> > map;
 	queue<int> q;
+//	int shortest_path_dist[][] = new int[5][5];
+	int **shortest_path_dist = new int*[V];
+	for(int i = 0; i < V; ++i) {
+	    shortest_path_dist[i] = new int[V];
+	}
+
 //	 typedef map<string,list<int> > maptype;
 
 //	    std::map<int, std::set<int> > mymap;
@@ -129,7 +154,7 @@ int main() {
 
 //	calculating the centrality
 	calculate_centrality(5, adj, predecessor, sigma, distance, delta, st, map,
-			q);
+			q,BC,shortest_path_dist);
 
 //	Printing the graph
 	graph_print(adj, V);
