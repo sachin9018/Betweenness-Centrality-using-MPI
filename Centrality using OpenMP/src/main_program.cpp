952e1b44dbc421176bffcd92618c64194494611a
ssgandham <gandhamsnehalsrinivas@gmail.com>
2018-02-21 23:22:22 -0800

    function parameters have been added

--- Centrality using OpenMP/src/main_program.cpp
+++ Centrality using OpenMP/src/main_program.cpp
@@ -15,37 +15,57 @@
 #include <vector>
 using namespace std;
 
-
-void edge_add(int src, int dest, list<int> adj[]) {
-	cout << src << ":" << dest<<"\n";
+void edge_add(int src, int dest, vector<int> adj[]) {
+	cout << src << ":" << dest << "\n";
 	adj[src].push_back(dest);
 	adj[dest].push_back(src);
 }
 
-void graph_print(list<int> adj[], int V){
+void graph_print(vector<int> adj[], int V) {
 
-	    for (int u = 0; u < V; u++)
-	    {
-	        cout << "Node " << u << " makes an edge with \n";
-	        for (auto it = adj[u].begin(); it!=adj[u].end(); ++it)
-	        {
-	        	cout<<*it <<":";
-	        }
-	        cout<<"\n";
-	        }
+	for (int u = 0; u < V; u++) {
+		cout << "Node " << u << " makes an edge with \n";
+		for (auto it = adj[u].begin(); it != adj[u].end(); ++it) {
+			cout << *it << ":";
+		}
+		cout << "\n";
+	}
 
 }
 
-//};
+void initialize(int source_vertex, vector<int> predecessor[], int *sigma, int *distance, float *delta){
+
+}
+void calculate_centrality(vector<int> adj[], int V,vector<int> predecessor[],int *sigma, int *distance,float *delta) {
+
+	//	itirating through each vertex
+	for(int i=1;i<=V;i++){
+		int source_vertex = i;
+		initialize(source_vertex,predecessor, sigma,distance,delta);
+	}
+}
 
 int main() {
 
+//	Declarations
 	int V = 5;
-	list<int> adj[V];
+	vector<int> adj[V];
+	vector<int> predecessor[V];
+	int *sigma = new int[V];
+	int *distance = new int[V];
+	float *delta = new float[V];
+
+
+//	Adding to the adjacency list
 	edge_add(1, 2, adj);
 	edge_add(2, 3, adj);
 	edge_add(3, 4, adj);
 	edge_add(4, 1, adj);
+
+//	calculating the centrality
+	calculate_centrality(adj,5,predecessor, sigma,distance,delta);
+
+//	Printing the graph
 	graph_print(adj, V);
 
 	return 0;
